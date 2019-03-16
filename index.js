3b75bb9236f3125dd964525ff09a358b858a0e91
Tyler Lentz <lentz.etyler@gmail.com>
2019-03-16 01:29:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,15 @@ function logWhisper(string) {
   console.log("hello")
 }
 
-var lowercase = 'I can\'t hear you!'
-lowercase.toLowerCase() === lowercase
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+if (string === lowercase) {
+  return "I can\'t hear you!"
+}else if (string === uppercase){
+  return "YES INDEED!"
+}else if (string === "I love you, Grandma."){
+  return "I love you, too."
+}
+}
+
